276d4f21e8f032c50aeb1231390edd220da8df32
Anay Roy <anayroy@Anays-MacBook-Air.local>
2024-07-22 21:20:28 +0530

    profile page completed

---
+++ src/constants/index.js
@@ -0,0 +1,45 @@
+import project1 from "../assets/projects/project-1.jpeg";
+import project2 from "../assets/projects/project-2.jpeg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpeg";
+
+export const HERO_CONTENT = `I am a budding full-stack developer enthusiastic about building dynamic web applications. With foundational knowledge in front-end technologies such as React and Flutter, and back-end technologies including Node.js and databases like MongoDB and MySQL, I am keen to explore the intersection of technology and innovation. I also have a growing interest in blockchain development and am beginning to delve into the exciting world of machine learning. My goal is to leverage these skills to create impactful solutions that enhance user experiences and drive technological advancements.`;
+
+export const ABOUT_TEXT = `I am an aspiring web developer with a keen interest in crafting efficient and user-friendly web applications. With a foundational understanding of full-stack development, I am proficient in front-end technologies like HTML, CSS, and JavaScript. I am also familiar with modern frameworks such as React and Flutter for building interactive user interfaces. On the back end, I have experience with Node.js and databases like MongoDB. My journey into web development began with a fascination for creating digital solutions, and I am eager to expand my skills in areas such as blockchain and machine learning to innovate in the field. I enjoy collaborative environments where I can contribute my problem-solving skills to deliver impactful solutions. Apart from coding, I actively explore new technologies and contribute to open-source projects to further enhance my expertise.`;
+
+export const PROJECTS = [
+  {
+    title: "NSE python bot",
+    image: project1,
+    description:
+      "A telegram bot that provides all the information about the stock listed on national stock exchange",
+    technologies: ["Python"],
+  },
+  {
+    title: "ResConnect",
+    image: project2,
+    description:
+      "An application that allows different type of rescue agencies to collaborate during the time of disaster",
+    technologies: ["Flutter", "Dart", "Google map api", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Vismit password manager",
+    image: project4,
+    description:
+      "An android/ios application that takes a key, website name and the length as input and uses hashing and slashing for generating password ",
+    technologies: ["Flutter", "Dart"],
+  },
+];
+
+export const CONTACT = {
+  address: "Bhubaneshwar, Odhisha ",
+  phoneNo: "+91-9693942893 ",
+  email: "anayroy@anayroy.online",
+};
