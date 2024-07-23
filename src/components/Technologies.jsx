81152d267f5ce9af06ae6a6130d7e6f9ce4d11f6
Anay Roy <anayroy@Anays-MacBook-Air.local>
2024-07-23 12:44:24 +0530

    about section completed

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import { RiReactjsLine } from 'react-icons/ri'
+import { BiLogoFlutter } from "react-icons/bi";
+import { SiDart } from "react-icons/si";
+import { IoLogoFirebase } from "react-icons/io5";
+import { SiMongodb } from "react-icons/si";
+import { FcAndroidOs } from "react-icons/fc";
+
 
 const Technologies = () => {
   return (
@@ -7,7 +13,22 @@ const Technologies = () => {
       <h2 className='my-20 text-center text-4xl'>Technologies</h2>
       <div className='flex flex-wrap item-center justify-center gap-4'>
         <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-            <RiReactjsLine />
+            <RiReactjsLine className='text-7xl text-cyan-400'/>
+        </div>
+        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <FcAndroidOs className='text-7xl text-cyan-400'/>
+        </div>
+        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <BiLogoFlutter className='text-7xl text-cyan-400'/>
+        </div>
+        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <SiDart className='text-7xl text-cyan-400'/>
+        </div>
+        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <IoLogoFirebase className='text-7xl text-yellow-400'/>
+        </div>
+        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <SiMongodb className='text-7xl text-green-400'/>
         </div>
       </div>
     </div>
