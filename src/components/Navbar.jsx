2e880ad3f3c34319c3f14bc24036e292bbe17c26
Anay Roy <anayroy@Anays-MacBook-Air.local>
2024-07-23 14:07:29 +0530

    completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import logo from "../assets/logo.png";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className=" mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 item-center">
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex items-center">
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
-      <div className="m-8 flex item-center justify-center gap-4 text-2xl">
-        <FaGithub/>
-        <FaLinkedin/>
-        <FaInstagram/>
+      <div className="flex items-center gap-4 text-2xl">
+        <a href="https://github.com/anay-roy" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/anay-roy-407a9b21a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://instagram.com/anay.roy1" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
